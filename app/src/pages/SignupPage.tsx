9ebdaf91790939185088b5658ecd2b6518d9ec04
Dechanuphap Anuwan <dechanuphap.a@kkumail.com>
2023-09-17 01:55:49 +0700

    redesign sign up page

--- app/src/pages/SignupPage.tsx
+++ app/src/pages/SignupPage.tsx
@@ -1,23 +1,49 @@
+import logo from "./../../public/logo/logo-900.svg";
+import { Link } from "react-router-dom";
+
 function SignupPage() {
     return (
-        <div className="h-screen flex justify-center items-center bg-gray-50">
-            <div className="bg-white p-6 rounded-lg shadow">
+        <div className="h-screen flex justify-center items-center relative">
+            <div className="h-screen w-screen overlay-background absolute top-0 left-0 -z-10"></div>
+
+            <div className="bg-white/90 px-16 py-10 rounded-lg shadow flex flex-col">
+                <div className="flex flex-col justify-center items-center gap-2 mb-8">
+                    <img src={logo} className="w-16 h-16"></img>
+                    <div className="font-bold text-xl">HermesCS</div>
+                </div>
+
                 <div className="mb-4">
-                    <input type="text" className="px-4 py-2 rounded-lg border" placeholder="Username"></input>
+                    <input
+                        type="text"
+                        className="px-4 py-2 rounded-lg border"
+                        placeholder="Username"
+                    ></input>
                 </div>
                 <div className="mb-4">
-                    <input type="email" className="px-4 py-2 rounded-lg border" placeholder="Username"></input>
+                    <input
+                        type="password"
+                        className="px-4 py-2 rounded-lg border"
+                        placeholder="Password"
+                    ></input>
                 </div>
                 <div className="mb-4">
-                    <input type="password" className="px-4 py-2 rounded-lg border" placeholder="Password"></input>
+                    <input
+                        type="password"
+                        className="px-4 py-2 rounded-lg border"
+                        placeholder="Confirm Password"
+                    ></input>
                 </div>
-                <div className="mb-4">
-                    <input type="password" className="px-4 py-2 rounded-lg border" placeholder="Confirm Password"></input>
+                <div className="my-6 relative">
+                    <button
+                        type="button"
+                        className="w-full px-4 py-2 rounded-lg bg-blue-500 text-white font-semibold"
+                    >
+                        Sign up
+                    </button>
                 </div>
-                <div className="mb-6 relative">
-                    <button type="button" className="w-full px-4 py-2 rounded-lg bg-blue-500 text-white font-semibold">Sign up</button>
-                </div>
-                <button className="tex-sm">Login</button>
+                <Link to={"/login"} className="flex justify-center">
+                    <button className="tex-sm">Back to Login</button>
+                </Link>
             </div>
         </div>
     );
