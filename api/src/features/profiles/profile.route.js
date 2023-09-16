908be35b80dd0f56c86ce19409c73a0d75a60653
VillSource <anirut1800@gmail.com>
2023-09-17 04:59:40 +0700

    feature: ⚙️ patch profile

--- api/src/features/profiles/profile.route.js
+++ api/src/features/profiles/profile.route.js
@@ -2,12 +2,18 @@ var express = require('express');
 const getProfileFromToken = require('./from-token');
 const authGuard = require('../../authorization/auth.middlewere');
 const getProfileFromId = require('./from-id');
+const patchDisplayNameEndpoint = require('./patch-display-name');
+const patchAvatarEndpoint = require('./patch-avatar');
 var router = express.Router();
 
 /* GET /profile */
 router.get('/',authGuard, getProfileFromToken);
 /* GET /profile/:id */
 router.get('/:id', getProfileFromId)
+/* PATCH /profile/displayname/:displayname */
+router.patch('/displayname/:displayname',authGuard, patchDisplayNameEndpoint);
+/* PATCH /profile/avatar */
+router.patch('/avatar',authGuard, patchAvatarEndpoint);
 
 module.exports = router;
 
