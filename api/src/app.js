e7f7dd6088b478f576867e8ff99d8206e22d9ce4
VillSource <anirut1800@gmail.com>
2023-09-24 01:12:45 +0700

    fix: 🛡️ auto send json by thow error and object

--- api/src/app.js
+++ api/src/app.js
@@ -31,7 +31,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", 'public')));
 
-app.use(ErrrorHandlerMiddleware);
 
 app.use('/', indexRouter);
 app.use('/account', authRouter);
@@ -44,16 +43,6 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-
+app.use(ErrrorHandlerMiddleware);
 
 module.exports = app;
--- api/src/middleware/EerrorHandler.middleware.js
+++ api/src/middleware/EerrorHandler.middleware.js
@@ -1,17 +1,14 @@
 const HTTP_STATUS = require("../core/value-object");
 
-const ErrrorHandler = async (req, res, next) =>{
-
-    try{
-        await next();
-    }
-    catch (e){
-        console.error("\x1b[41m%s\n\x1b[0m","!ERROR!", e);
-        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-            error : "Internal Server Error"
-        });
-    }
-
+function ErrrorHandler(err, req, res, next) {
+    console.error("\x1b[41m%s\n\x1b[0m", "!ERROR!", err);
+    return !(err instanceof Error)
+        ? res.status(HTTP_STATUS.BAD_REQUEST).json(err)
+        : res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            error: "Internal Server Error",
+            msg: err.message ?? null,
+            stack: req.app.get('env') === 'development' ? err.stack?.split("\n") : null
+        })
 }
 
 module.exports = ErrrorHandler;
