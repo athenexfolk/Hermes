6f3beb2195b7734591031be61e31d86a4a97c3e7
VillSource <anirut1800@gmail.com>
2023-09-24 21:07:49 +0700

    feature: 🏬cache profile req

---
+++ app-ng/src/app/interceptor/cache.interceptor.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, finalize, shareReplay } from 'rxjs';
+import { enviroment } from 'src/enviroment/enviroment.dev';
+
+@Injectable()
+export class CacheProfileInterceptor implements HttpInterceptor {
+
+  private cache = new Map<URL, Observable<HttpEvent<unknown>>>();
+  private baseUrl = new URL(enviroment.API_SERVER_URL);
+
+  constructor() { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const url = new URL(request.urlWithParams);
+
+    if (request.method !== 'GET') {
+      return next.handle(request);
+    }
+
+    if (!request.url.startsWith(new URL('/profile',this.baseUrl).toString()))
+      return next.handle(request);
+
+    console.debug("CacheInterceptor intercept : ", request.urlWithParams);
+
+    if (!this.cache.has(url)) {
+      const response = next.handle(request).pipe(
+        finalize(() => this.cache.delete(url)),
+        shareReplay({ refCount: true, bufferSize: 1 })
+      );
+      this.cache.set(url, response);
+    }
+
+    return this.cache.get(url)!;
+  }
+}
