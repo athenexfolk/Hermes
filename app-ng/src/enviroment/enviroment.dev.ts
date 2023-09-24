046eff9556096b2b9e7e0484a757c646d99bc3f7
VillSource <anirut1800@gmail.com>
2023-09-25 03:00:23 +0700

    deploy: add docker config

--- app-ng/src/enviroment/enviroment.dev.ts
+++ app-ng/src/enviroment/enviroment.dev.ts
@@ -1,5 +1,8 @@
 export const enviroment = {
-  API_SERVER_URL: 'http://localhost:3000',
-  CHAT_PORTAL_URL: 'http://localhost:3000/chat',
+  // API_SERVER_URL: 'http://localhost:3000',
+  // CHAT_PORTAL_URL: 'http://localhost:3000/chat',
+
+  API_SERVER_URL: 'http://localhost/api',
+  CHAT_PORTAL_URL: 'http://localhost/api/chat',
   TOKEN_KEY: 'token',
 }
