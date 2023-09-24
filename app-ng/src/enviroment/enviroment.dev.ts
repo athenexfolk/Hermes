dd78dcc69a7d67e71b6c96a17c348dcfc57fd363
VillSource <anirut1800@gmail.com>
2023-09-25 05:11:42 +0700

    chang origin http -> https

--- app-ng/src/enviroment/enviroment.dev.ts
+++ app-ng/src/enviroment/enviroment.dev.ts
@@ -2,7 +2,7 @@ export const enviroment = {
   // API_SERVER_URL: 'http://localhost:3000',
   // CHAT_PORTAL_URL: 'http://localhost:3000/chat',
 
-  API_SERVER_URL: 'http://p.villsource.tk/api',
-  CHAT_PORTAL_URL: 'http://p.villsource.tk/api/chat',
+  API_SERVER_URL: 'https://p.villsource.tk/api',
+  CHAT_PORTAL_URL: 'https://p.villsource.tk/api/chat',
   TOKEN_KEY: 'token',
 }
