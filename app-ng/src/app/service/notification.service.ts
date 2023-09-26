7a81bdeb41ea3019c9c13a2239614c551d60f19f
VillSource <anirut1800@gmail.com>
2023-09-26 22:01:19 +0700

    fix: stop alert none support

--- app-ng/src/app/service/notification.service.ts
+++ app-ng/src/app/service/notification.service.ts
@@ -14,14 +14,13 @@ export class NotificationService {
   notifyMe(data: MessageDto) {
     if (data.chatContent.type.toString() === 'welcome_message') return;
     this.prifile.getProfile(data.sender).subscribe(u => {
-
       const opt: NotificationOptions = {
         body: data.chatContent.type.toString() === 'text' ? data.chatContent.value : undefined,
         image: data.chatContent.type.toString() === 'image' ? data.chatContent.value : undefined,
         icon: u.avatar
       }
       if (!("Notification" in window)) {
-        alert("This browser does not support desktop notification");
+        // alert("This browser does not support desktop notification");
       } else if (Notification.permission === "granted") {
         const notification = new Notification(u.displayName,opt);
       } else if (Notification.permission !== "denied") {
